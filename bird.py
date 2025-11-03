35363cd999f30754b58877377381025d204a2613
Kang-Jiho79 <giho2630@gmail.com>
2025-11-03 20:47:48 +0900

    update bird __init__ 완성

--- bird.py
+++ bird.py
@@ -1,5 +1,7 @@
+from pico2d import *
 import game_world
 import game_framework
+import random
 
 PIXEL_PER_METER = (140.0 / 0.18) # 140 pixel 18 cm
 BIRD_FLY_SPEED_KMPH = 38.0 # Km / Hour
@@ -14,4 +16,10 @@ FRAMES_PER_ACTION = 14
 class Bird:
     image = None
     def __init__(self):
-        pass
+        self.x = random.randint(0, 1600)
+        self.y = random.randint(300, 600)
+        self.frame = 0
+        self.face_dir = 1
+        self.dir = 0
+        if Bird.image == None:
+            Bird.image = load_image('bird_animation.png')
